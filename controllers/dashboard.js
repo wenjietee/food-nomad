63f7c02219cb81c5589a4f68a705a30872afcb63
Wenjie <wenjietj@gmail.com>
2021-02-24 22:15:39 +0800

    added recipe schema

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/users');
+const Recipe = require('../models/recipe');
 
 // MIDDLEWARE
 
---
+++ models/recipe.js
@@ -0,0 +1,17 @@
+// DEPENDENCIES
+const mongoose = require('mongoose');
+
+// SCHEMA
+const Schema = mongoose.Schema;
+
+const recipeSchema = Schema({
+	name: { type: String, required: true },
+	author: { type: String, required: true },
+	isRecipe: Boolean,
+	ingredients: [String],
+	instructions: { type: String, required: true },
+});
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+module.exports = Recipe;

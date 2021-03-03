7a3344bc6e5eb30592f0c043a06c38ba91a54a74
Wenjie <wenjietj@gmail.com>
2021-03-04 00:40:02 +0800

    google maps can be accessed from map route

---
+++ public/scripts/map.js
@@ -0,0 +1,12 @@
+////////////////////
+// Init Google Maps
+////////////////////
+
+let map;
+
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: { lat: -34.397, lng: 150.644 },
+		zoom: 8,
+	});
+}

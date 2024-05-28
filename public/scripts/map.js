e3ad562f538010c0408177cd2fe2be7d32970d82
Tee Wenjie <wenjietj@gmail.com>
2024-05-28 23:06:07 +0800

    update packages and onemap api and json

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -4,75 +4,75 @@
 */
 
 function initMap() {
-	// set default location
-	const SG = { lat: 1.29027, lng: 103.851959 };
+    // set default location
+    const SG = { lat: 1.29027, lng: 103.851959 };
 
-	// create new map object and center on user's location
-	const map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 17,
-		center: SG,
-	});
+    // create new map object and center on user's location
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 17,
+        center: SG,
+    });
 
-	// get food and user data from server
-	$.ajax({
-		url: '/food/data',
-	}).then((data) => {
-		// store data
-		const currentUser = data.currentUser;
-		const foods = data.foods;
-		const users = data.users;
+    // get food and user data from server
+    $.ajax({
+        url: "/food/data",
+    }).then((data) => {
+        // store data
+        const currentUser = data.currentUser;
+        const foods = data.foods;
+        const users = data.users;
 
-		// iterate food authors' zip code and convert it into lat longs
-		users.forEach((user) => {
-			$.ajax({
-				url: `https://developers.onemap.sg/commonapi/search?searchVal=${user.zip}&returnGeom=Y&getAddrDetails=Y&pageNum=1`,
-			}).then((zip) => {
-				// store user location
-				const userLocation = {
-					lat: Number(zip.results[0].LATITUDE),
-					lng: Number(zip.results[0].LONGTITUDE),
-				};
+        // iterate food authors' zip code and convert it into lat longs
+        users.forEach((user) => {
+            $.ajax({
+                url: `https://www.onemap.gov.sg/api/common/elastic/search?searchVal=${user.zip}&returnGeom=Y&getAddrDetails=Y&pageNum=1`,
+            }).then((zip) => {
+                // store user location
+                const userLocation = {
+                    lat: Number(zip.results[0].LATITUDE),
+                    lng: Number(zip.results[0].LONGITUDE),
+                };
 
-				// username match current user center map on user
-				if (user.username === currentUser.username) {
-					map.setOptions({
-						center: userLocation,
-					});
-				}
+                // username match current user center map on user
+                if (user.username === currentUser.username) {
+                    map.setOptions({
+                        center: userLocation,
+                    });
+                }
 
-				const contentString =
-					'<div id="map-info">' +
-					'<a href="/app/profile/' +
-					user.username +
-					'">' +
-					'<h6>' +
-					user.username +
-					'</h6>' +
-					'</a>' +
-					'<h6>is sharing!</h6>' +
-					'<p>Email: ' +
-					user.email +
-					'</p>' +
-					'</div>';
+                const contentString =
+                    '<div id="map-info">' +
+                    '<a href="/app/profile/' +
+                    user.username +
+                    '">' +
+                    "<h6>" +
+                    user.username +
+                    "</h6>" +
+                    "</a>" +
+                    "<h6>is sharing!</h6>" +
+                    "<p>Email: " +
+                    user.email +
+                    "</p>" +
+                    "</div>";
 
-				// create info window
-				const infowindow = new google.maps.InfoWindow({
-					content: contentString,
-				});
+                // create info window
+                const infowindow = new google.maps.InfoWindow({
+                    content: contentString,
+                });
 
-				// create marker
-				const marker = new google.maps.Marker({
-					position: userLocation,
-					map,
-				});
+                // create marker
+                const marker = new google.maps.Marker({
+                    position: userLocation,
+                    map,
+                });
 
-				marker.addListener('click', () => {
-					infowindow.open(map, marker);
-				});
-			});
-		});
-	}),
-		() => {
-			console.log('bad request');
-		};
+                marker.addListener("click", () => {
+                    infowindow.open(map, marker);
+                });
+            });
+        });
+    }),
+        () => {
+            console.log("bad request");
+        };
 }
--- server.js
+++ server.js
@@ -1,55 +1,55 @@
 // DEPENDENCIES
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const session = require('express-session');
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
+const session = require("express-session");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 
 // CONTROLLER DEPENDENCIES
-const userController = require('./controllers/users');
-const sessionsController = require('./controllers/sessions');
-const appController = require('./controllers/app');
-const foodShareController = require('./controllers/foodshare');
+const userController = require("./controllers/users");
+const sessionsController = require("./controllers/sessions");
+const appController = require("./controllers/app");
+const foodShareController = require("./controllers/foodshare");
 //const devController = require('./controllers/dev');
 
 // CONFIG
 const PORT = process.env.PORT || 3000;
 const MONGOURI = process.env.MONGOATLAS || process.env.MONGOLOCAL;
 const SESSIONS_CONFIG = {
-	secret: process.env.SECRET,
-	resave: false,
-	saveUninitialized: false,
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
 };
 
 // MIDDLEWARE
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(session(SESSIONS_CONFIG));
 
 // DATABASE ( MONGOOSE / MONGODB )
 mongoose.connect(MONGOURI, { useNewUrlParser: true });
-mongoose.connection.once('open', () => {
-	console.log('connected to mongo');
+mongoose.connection.once("open", () => {
+    console.log("connected to mongo");
 });
 
 //CONTROLLER ROUTES
-app.use('/users', userController);
-app.use('/sessions', sessionsController);
-app.use('/app', appController);
-app.use('/food', foodShareController);
+app.use("/users", userController);
+app.use("/sessions", sessionsController);
+app.use("/app", appController);
+app.use("/food", foodShareController);
 //app.use('/dev', devController); //for dev testing
 
 // ROUTES
 
 // main index
-app.get('/', (req, res) => {
-	res.render('index.ejs');
+app.get("/", (req, res) => {
+    res.render("index.ejs");
 });
 
 // LISTEN
 app.listen(PORT, () => {
-	console.log(`listening on port:${PORT}`);
+    console.log(`listening on port:${PORT}`);
 });
